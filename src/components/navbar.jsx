a217202dede60bf25397aa97e8292347f0915482
lordeber15 <eberpalomino@hotmail.com>
2024-03-27 18:48:05 -0500

    corecciones de diseño

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 function Navbar() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-row justify-between h-20 items-center px-20 bg-teal-600">
+    <div className="flex flex-row justify-between h-20 items-center px-20 bg-teal-600 w-screen">
       <Link to="/dashboard" className="flex gap-3">
         <img src={logo} className=" h-14" />
         <div className="flex font-bold text-white text-2xl items-center">
@@ -20,7 +20,7 @@ function Navbar() {
           }}
           className="relative group"
         >
-          <div className="relative flex flex-col overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-teal-500 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+          <div className="relative flex flex-col overflow-hidden items-center justify-center rounded-full w-[30px] h-[30px] transform transition-all bg-teal-500 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
             <div className="transform transition-all duration-150 overflow-hidden -translate-y-5 group-focus:translate-y-3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -38,14 +38,13 @@ function Navbar() {
               </svg>
             </div>
 
-            <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden -translate-y-3">
-              <div className="bg-white mb-1.5 h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6"></div>
-              <div className="bg-white mb-1.5 h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-y-6 delay-75"></div>
-              <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6 delay-100"></div>
+            <div className="flex flex-col justify-between ml-2 mt-2 mb-0 w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden -translate-y-3">
+              <div className="bg-white mb-1.2 h-[2px] w-3 transform transition-all duration-300 origin-left group-focus:translate-y-6"></div>
+              <div className="bg-white mb-1.2 h-[2px] w-3 rounded transform transition-all duration-300 group-focus:translate-y-6 delay-75"></div>
+              <div className="bg-white h-[2px] w-3 transform transition-all duration-300 origin-left group-focus:translate-y-6 delay-100"></div>
             </div>
           </div>
         </button>
-        <button>-</button>
       </div>
     </div>
   );
--- src/page/historias.jsx
+++ src/page/historias.jsx
@@ -2,15 +2,27 @@ import { useNavigate } from "react-router-dom";
 function Historias() {
   const navigate = useNavigate();
   return (
-    <div className="w-screen px-40 py-10 flex gap-3 flex-col">
+    <div className="w-screen px-28 py-10 flex gap-3 flex-col">
       <div className="flex justify-between">
         <button
           onClick={() => {
             navigate("/dashboard");
           }}
-          className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-1 px-2 rounded"
         >
-          {"<"}
+          <svg
+            className="h-8 w-8 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
         </button>
         <h1 className="text-3xl font-bold flex justify-center text-teal-900">
           Historias Clinicas
@@ -19,97 +31,184 @@ function Historias() {
       </div>
 
       <div className="flex gap-3 justify-end">
-        <input
-          className="w-[15%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="number"
-          min={0}
-          placeholder="H.C N°"
-        />
-        <input
-          className="w-[15%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="FECHA"
-        />
+        <div>
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            H.C N°
+          </label>
+          <input
+            className="w-full text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="number"
+            min={0}
+            placeholder="H.C N°"
+          />
+        </div>
+        <div>
+          <label className=" font-bold text-sm px-1 text-teal-600">FECHA</label>
+          <input
+            className="w-full text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="dd/mm/yy"
+          />
+        </div>
       </div>
-      <h2 className="text-2xl font-bold text-teal-900">Datos de Afiliacion</h2>
-      <div className="flex gap-3">
-        <input
-          className="w-[70%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Nombre y Apellido"
-        />
-        <input
-          className="w-[30%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="number"
-          placeholder="Edad"
-        />
+      <div className="flex justify-between ">
+        <h2 className="text-2xl font-bold text-teal-900">
+          Datos de Afiliacion
+        </h2>
+        <select className=" w-1/4 py-2 px-3 font-semibold  focus:outline-none border rounded focus:shadow-outline shadow appearance-none">
+          <option>Adulto</option>
+          <option>Niño</option>
+          <option>Ortodoncia</option>
+        </select>
       </div>
       <div className="flex gap-3">
-        <input
-          className="w-[50%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Direccion"
-        />
-        <input
-          className="w-[50%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Ref"
-        />
+        <div className="flex flex-col w-full">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Nombre y Apellidos
+          </label>
+          <input
+            className="text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Nombre y Apellido"
+          />
+        </div>
+        <div className="flex flex-col w-[30%]">
+          <label className=" font-bold text-sm px-1 text-teal-600">Edad</label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="number"
+            placeholder="Edad"
+          />
+        </div>
+      </div>
+      <div className="flex gap-3">
+        <div className="flex flex-col w-[50%]">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Dirección
+          </label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Direccion"
+          />
+        </div>
+        <div className="flex flex-col w-[50%]">
+          <label className=" font-bold text-sm px-1 text-teal-600">Ref</label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Ref"
+          />
+        </div>
       </div>
       <div className="flex gap-3 flex-wrap">
-        <input
-          className=" w-[24.3%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="number"
-          min={0}
-          placeholder="Doc Ident"
-        />
-        <input
-          className=" w-[24.2%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Est Civil"
-        />
-        <input
-          className=" w-[24%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="number"
-          min={0}
-          placeholder="Telf. Fijo"
-        />
-        <input
-          className=" w-[24%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="number"
-          placeholder="Cel"
-          min={0}
-        />
+        <div className="flex flex-col w-[24%]">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Doc Ident
+          </label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="number"
+            min={0}
+            maxLength={8}
+            placeholder="Doc Ident"
+          />
+        </div>
+        <div className="flex flex-col w-[24.5%]">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Est Civil
+          </label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Est Civil"
+          />
+        </div>
+        <div className="flex flex-col w-[24%]">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Telf. Fijo
+          </label>
+          <input
+            className="  text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="number"
+            maxLength={9}
+            min={0}
+            placeholder="Telf. Fijo"
+          />
+        </div>
+        <div className="flex flex-col w-[24%]">
+          <label className=" font-bold text-sm px-1 text-teal-600">Cel</label>
+          <input
+            className="  text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="number"
+            maxLength={9}
+            placeholder="Cel"
+            min={0}
+          />
+        </div>
+      </div>
+      <div className="flex gap-3 w-full">
+        <div className="flex flex-col w-1/2">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Operacion
+          </label>
+          <input
+            className="text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Operacion"
+          />
+        </div>
+        <div className="flex flex-col w-1/2">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Lugar de Procedencia
+          </label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Lugar de Procedencia"
+          />
+        </div>
       </div>
       <div className="flex gap-3">
+        <div className="flex flex-col w-1/2">
+          <label className=" font-bold text-sm px-1 text-teal-600">Email</label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Email"
+          />
+        </div>
+        <div className="flex flex-col w-1/2">
+          <label className=" font-bold text-sm px-1 text-teal-600">
+            Antecedentes patologicos (personales y/o familiares)
+          </label>
+          <input
+            className=" text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Antecedentes patologicos (personales y/o familiares)"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col w-full">
+        <label className=" font-bold text-sm px-1 text-teal-600">
+          Alergio a algun medicamento
+        </label>
         <input
-          className="w-[50%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          className="w-full text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
-          placeholder="Operacion"
-        />
-        <input
-          className="w-[50%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Lugar de Procedencia"
+          placeholder="Alergio a algun medicamento"
         />
       </div>
-      <div className="flex gap-3">
+      <div className="flex flex-col w-full ">
+        <label className=" w-1/4 font-bold text-sm px-1 text-teal-600">
+          C.O.P.
+        </label>
         <input
-          className="w-[50%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          className="w-1/4 flex text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
-          placeholder="Email"
-        />
-        <input
-          className="w-[50%] text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Antecedentes patologicos (personales y/o familiares)"
+          placeholder="C.O.P."
         />
       </div>
-      <input
-        className="w-full text-teal-800 font-semibold shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
-        placeholder="Alergio a algun medicamento"
-      />
       <div className="w-full flex justify-end">
         <button className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded flex w-32 justify-center">
           Guardar
