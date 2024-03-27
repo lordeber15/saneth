58a271bd78f609853c4a33151a17bf0660c54983
lordeber15 <eberpalomino@hotmail.com>
2024-03-26 19:49:17 -0500

    historias2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./page/login";
 import Dashboard from "./page/dashboard";
-import Historias from "./page/historias";
 import Navbar from "./components/navbar";
 import { useLocation } from "react-router-dom";
 
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/historias" element={<Historias />} />
       </Routes>
     </div>
   );
